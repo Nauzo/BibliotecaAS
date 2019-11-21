7ba08615c5d550159188b99fe8eedc3db415f164
Nauzo <juan.ortega850291@gmail.com>
2019-11-21 13:28:45 -0300

    eh agregado la clase Base_act y AdminSQLiteOpenHelper para la creacion de la bd y su administración.

---
+++ app/src/main/java/com/example/bibliotecajo/AdminSQLiteOpenHelper.java
@@ -0,0 +1,27 @@
+package com.example.bibliotecajo;
+
+import android.content.Context;
+import  android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
+
+
+    public AdminSQLiteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase BaseDeDatosLibros) {
+
+        BaseDeDatosLibros.execSQL("CREATE TABLE Libros(codigo_id int primary key, nombre text, precio float)");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
